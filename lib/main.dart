c57c08407997ceec1bc374fd475d2730391f2b11
kareem fawas <phawazzzy@gmail.com>
2020-04-29 23:55:37 +0100

    first screen almost done

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Trippas',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
-      home: Material(child: Scaffold(body: HomePage())),
+      home: Material(
+          child: Scaffold(
+        body: HomePage(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+
+          },
+          child: Icon(Icons.add),
+        ),
+      )),
     );
   }
 }
@@ -29,7 +38,7 @@ class _HomePageState extends State<HomePage> {
       child: Container(
         color: Colors.white,
         padding:
-            EdgeInsets.only(top: 24.0, left: 27.0, bottom: 24.0, right: 19.0),
+            EdgeInsets.only(top: 24.0, left: 27.0, right: 19.0),
         child: Column(
           children: <Widget>[
             Row(
@@ -83,7 +92,19 @@ class _HomePageState extends State<HomePage> {
             SizedBox(
               height: 20.0,
             ),
-            Flights()
+            Expanded(
+              child: ListView.separated(
+                itemCount: 20,
+                itemBuilder: (BuildContext context, int index) {
+                  return Flights();
+                },
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(
+                      height: 20.0,
+                      color: Colors.white
+                    ),
+              ),
+            )
           ],
         ),
       ),
@@ -99,108 +120,120 @@ class Flights extends StatefulWidget {
 class _FlightsState extends State<Flights> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200.0,
-      padding: EdgeInsets.all(10.0),
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(color: Colors.grey, blurRadius: 25.0, spreadRadius: 5.0)
-        ],
-        shape: BoxShape.rectangle,
-        border: Border.all(
-          color: Colors.black12,
-          width: 3,
-        ),
-        borderRadius: BorderRadius.circular(12),
-      ),
+    return Card(
+//      height: 200.0,
+//      padding: EdgeInsets.all(10.0),
+//      decoration: BoxDecoration(
+////        boxShadow: [
+////          BoxShadow(color: Colors.grey, blurRadius: 25.0, spreadRadius: 5.0)
+////        ],
+//        shape: BoxShape.rectangle,
+//        border: Border.all(
+////          color: Colors.black12,
+//          width: 1,
+//        ),
+//        borderRadius: BorderRadius.circular(12),
+//      ),
 //
-      child: Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                'Lagos',
-                style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'nunito-bold'),
-              ),
-              Icon(FlatIcons.paper_plane_1),
-              Text(
-                'London',
-                style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'nunito-bold'),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                'Mon, 23 2020',
-                style: TextStyle(
-                    fontSize: 11.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'nunito-regular',
-                    color: Colors.white54,
-                    letterSpacing: 2.0),
-              ),
-              Text(
-                'Tue, 24 2020',
-                style: TextStyle(
-                    fontSize: 11.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'nunito-regular',
-                    letterSpacing: 2.0),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                '12.24 pm',
-                style: TextStyle(
-                    fontSize: 11.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'nunito-regular',
-                    letterSpacing: 2.0),
-              ),
-              Text(
-                '1.24 pm',
-                style: TextStyle(
-                    fontSize: 11.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'nunito-regular',
-                    letterSpacing: 2.0),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Container(
-//                decoration: BoxDecoration(
-////                  borderRadius: BoxBorder(),
-//                ),
-                height: 20.0,
-                width: 50.0,
-                color: Colors.blue,
-                child: Center(
-                  child: Text(
-                    'berlin',
-                    style: TextStyle(color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Lagos',
+                  style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'nunito-bold'),
+                ),
+                Icon(FlatIcons.paper_plane_1),
+                Text(
+                  'London',
+                  style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'nunito-bold'),
+                )
+              ],
+            ),
+            SizedBox(height: 5.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Mon, 23 2020',
+                  style: TextStyle(
+                      fontSize: 11.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'nunito-regular',
+                      letterSpacing: 2.0),
+                ),
+                Text(
+                  'Tue, 24 2020',
+                  style: TextStyle(
+                      fontSize: 11.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'nunito-regular',
+                      letterSpacing: 2.0),
+                )
+              ],
+            ),
+            SizedBox(height: 5.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  '12.24 pm',
+                  style: TextStyle(
+                      fontSize: 11.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'nunito-regular',
+                      letterSpacing: 2.0),
+                ),
+                Text(
+                  '1.24 pm',
+                  style: TextStyle(
+                      fontSize: 11.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'nunito-regular',
+                      letterSpacing: 2.0),
+                )
+              ],
+            ),
+            SizedBox(height: 5.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  height: 20.0,
+                  width: 60.0,
+                  child: Center(
+                    child: Text(
+                      'Business',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
                     ),
                   ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5.0),
+                      ),
+                      color: Colors.blue),
                 ),
-              ),
-//              FlatButton.icon(onPressed: null, icon: Icon(FlatIcons.more_1), label: null)
-            ],
-          ),
-        ],
+                IconButton(
+                    icon: Icon(FlatIcons.more_1,),
+                    onPressed: () {
+                      print('this is the button');
+                    })
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
